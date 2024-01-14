3a3178ac6cdd2f4b41ef7fe0d698cd2bb16af3dc
unknown <2k22.cse.2213081@gmail.com>
2024-01-14 23:15:04 +0530

    Add README file

---
+++ js/index1.js
@@ -0,0 +1,5 @@
+// script.js (No changes from the previous example)
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Your message has been sent! We will get back to you soon.');
+});
