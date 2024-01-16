147129f6fd9c9619ead4eb468da698a82973f691
unknown <2k22.cse.2213081@gmail.com>
2024-01-16 19:39:37 +0530

    Done with the portfolio

--- js/index1.js
+++
@@ -1,5 +0,0 @@
-// script.js (No changes from the previous example)
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Your message has been sent! We will get back to you soon.');
-});
